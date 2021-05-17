ddca18f199a97d4a948d11064d3dbe1e0587e209
Cornelia Kelinske <corneliakelinske@gmail.com>
2021-05-17 10:07:14 -0700

    fixed error in app.js

--- assets/js/app.js
+++ assets/js/app.js
@@ -14,6 +14,6 @@ import "../css/app.scss"
 //
 import "phoenix_html"
 import socket from "./socket"
-import Video from ".video"
+import Video from "./video"
 
 Video.init(socket, document.getElementById("video"))
